927f6924d8f2d293c8ff334d8afe4afeb359329e
Giulia Ghisini <giulia.ghisini@redturtle.it>
2022-01-11 12:40:17 +0100

    feat: added script files

---
+++ scripts/update-dvt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -n "Enter design-volto-theme version: "
+read -r dvtVersion
+
+npx purge-lockfile@latest volto && yarn add "redturtle/design-volto-theme#v$dvtVersion" -W && yarn i18n && yarn add . &&  git commit -am "chore(upd8): update dvt v$dvtVersion"
