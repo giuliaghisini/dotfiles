098e19f04e4b8003438577e91b65c1b6cfedb35b
Giulia Ghisini <giulia.ghisini@redturtle.it>
2022-01-11 15:37:28 +0100

    fix: fixed update-dvt script

--- scripts/update-dvt.sh
+++ scripts/update-dvt.sh
@@ -3,4 +3,4 @@
 echo -n "Enter design-volto-theme version: "
 read -r dvtVersion
 
-npx purge-lockfile@latest volto && yarn add "redturtle/design-volto-theme#v$dvtVersion" -W && yarn i18n && yarn add . &&  git commit -am "chore(upd8): update dvt v$dvtVersion"
+npx purge-lockfile@latest volto && yarn add "redturtle/design-volto-theme#v$dvtVersion" -W && yarn i18n && git add . &&  git commit -am "chore(upd8): update dvt v$dvtVersion"
